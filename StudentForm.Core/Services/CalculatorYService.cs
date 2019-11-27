4c5ec42173735ea7ccf59d5656b35f6ffec3d9d0
d <d@DESKTOP-0INN7AI>
2019-11-26 21:04:34 -0500

    App Finished

--- StudentForm.Core/Services/CalculatorYService.cs
+++ StudentForm.Core/Services/CalculatorYService.cs
@@ -6,9 +6,10 @@ namespace StudentForm.Core.Services
 {
     public class CalculatorYService : ICalculatorService
     {
-        public double StudentFormValues(double x1, double x2,double a, double b, double c)
+        public double StudentFormValues(double x1, double x2, double a, double b, double c)
         {
-            x2 = (-b - (Math.Sqrt((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
+            double formula = b * b - 4.0 * a * c;
+            x2 = (-b + Math.Sqrt(formula)) / (2 * a );
             return x2;
         }
     }
--- StudentForm.Core/ViewModels/StudentFormViewModel.cs
+++ StudentForm.Core/ViewModels/StudentFormViewModel.cs
@@ -1,9 +1,11 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
 using StudentForm.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace StudentForm.Core.ViewModels
 {
@@ -15,6 +17,7 @@ namespace StudentForm.Core.ViewModels
         private double _c;
         private double _x1;
         private double _x2;
+        private MvxCommand _calculateCommand;
 
         public StudentFormViewModel(ICalculatorService calculatorService)
         {
@@ -24,54 +27,38 @@ namespace StudentForm.Core.ViewModels
         public double a
         {
             get => _a;
-            set
-            {
-                _a = value;
-                RaisePropertyChanged(() => a);
-                Recalculate();
-            }
+            set => SetProperty(ref _a, value);
         }
 
         public double b
         {
             get => _b;
-            set
-            {
-                _b = value;
-                RaisePropertyChanged(() => b);
-                Recalculate();
-            }
+            set => SetProperty(ref _b, value);
         }
 
         public double c
         {
             get => _c;
-            set
-            {
-                _c = value;
-                RaisePropertyChanged(() => c);
-                Recalculate();
-            }
+            set => SetProperty(ref _c, value);
         }
         public double x1
         {
             get => _x1;
-            set
-            {
-                _x1 = value;
-                RaisePropertyChanged(() => _x1);
-                Recalculate();
-            }
+            set => SetProperty(ref _x1, value);
         }
 
         public double x2
         {
             get => _x2;
-            set
+            set => SetProperty(ref _x2, value);
+        }
+
+        public ICommand CalculateCommand
+        {
+            get
             {
-                _x2 = value;
-                RaisePropertyChanged(() => _x2);
-                Recalculate();
+                _calculateCommand = _calculateCommand ?? new MvxCommand(Recalculate);
+                return _calculateCommand;
             }
         }
 
@@ -90,7 +77,8 @@ namespace StudentForm.Core.ViewModels
 
         private void Recalculate()
         {
-            x1 = _calculatorService.StudentFormValues(x1,x1,a,b,c);
+            x1 = _calculatorService.StudentFormValues(x1,x2,a,b,c);
+            x2 = _calculatorService.StudentFormValues(x1, x2, a, b, c);
         }
     }
 }
